9cc3db9b637d767fd3492c38f2acefce9c642f6c
bbbng <heoseonho@gmail.com>
2025-02-10 22:04:31 +0900

    feat: enhance user registration to support profile image upload to S3

--- src/main/java/project2/controller/UserController.java
+++ src/main/java/project2/controller/UserController.java
@@ -3,18 +3,17 @@ package project2.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import lombok.RequiredArgsConstructor;
 import project2.dto.UserRegistrationDto;
 import project2.entity.Users;
 import project2.service.UserService;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RequiredArgsConstructor
@@ -24,8 +23,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/register") // 회원 가입 API
-    public ResponseEntity<String> apiRegisterUserAccount(@RequestBody UserRegistrationDto registrationDto) {
-        userService.registerUserAccount(registrationDto);
+    public ResponseEntity<String> apiRegisterUserAccount(@RequestBody UserRegistrationDto registrationDto,
+            @RequestPart(value = "profileImage", required = false) MultipartFile profileImage) {
+        userService.registerUserAccount(registrationDto, profileImage);
         return new ResponseEntity<>("회원 가입이 완료되었습니다.", HttpStatus.CREATED);
     }
 
--- src/main/java/project2/service/UserService.java
+++ src/main/java/project2/service/UserService.java
@@ -1,11 +1,23 @@
 package project2.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import lombok.RequiredArgsConstructor;
 import project2.dto.UserRegistrationDto;
 import project2.entity.Users;
+import project2.exception.ImageUploadException;
 import project2.exception.UserNotFoundException;
 import project2.repository.UserRepository;
 
@@ -15,13 +27,37 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final AmazonS3 amazonS3;
 
-    public Users registerUserAccount(UserRegistrationDto registrationDto) { // 회원 정보 저장
+    @Value("${cloud.aws.s3.bucket}")
+	private String bucketName;
+
+    public Users registerUserAccount(UserRegistrationDto registrationDto, MultipartFile image) { // 회원 정보 저장
         Users user = new Users();
+        String imageUrl = null;
+
+        // 이미지 파일이 존재하면 S3에 업로드
+        if (image != null && !image.isEmpty()) {
+            // users-UUID-원본파일명 형식으로 파일 저장
+            String fileName = "users/" + UUID.randomUUID() + "-" + image.getOriginalFilename();
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(image.getSize());
+            try {
+                amazonS3.putObject(new PutObjectRequest(bucketName, fileName, image.getInputStream(), metadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch (IOException e) {
+                throw new ImageUploadException("Failed to upload new image to S3", e);
+            } catch (AmazonClientException e) {
+                throw new ImageUploadException("Error communicating with S3", e);
+            }
+            imageUrl = amazonS3.getUrl(bucketName, fileName).toString();
+        }
+
         user.setUsername(registrationDto.getUsername());
         user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
         user.setEmail(registrationDto.getEmail());
-        // 프로필 이미지 url 정보 저장용 코드 작성 필요
+        user.setProfileImageUrl(imageUrl);
+        user.setBio(registrationDto.getBio());
         return userRepository.save(user);
     }
     
