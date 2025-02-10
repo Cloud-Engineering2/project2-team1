08196109a7ae95e76244eb3438589997b701a606
bbbng <heoseonho@gmail.com>
2025-02-10 17:03:14 +0900

    feat: add ModelAttribute and PathVariable imports to UserController

--- src/main/java/project2/controller/UserController.java
+++ src/main/java/project2/controller/UserController.java
@@ -3,6 +3,8 @@ package project2.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
