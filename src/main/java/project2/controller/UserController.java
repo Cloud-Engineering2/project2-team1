890e1b38299b76b9f01c1d9f60444da3ccb23b4c
bbbng <heoseonho@gmail.com>
2025-02-10 11:36:24 +0900

    refactor: convert UserController to RESTful API and update user registration handling

--- src/main/java/project2/controller/UserController.java
+++ src/main/java/project2/controller/UserController.java
@@ -1,31 +1,26 @@
 package project2.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 import project2.dto.UserRegistrationDto;
 import project2.service.UserService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class UserController {
     
     private final UserService userService;
 
-    @GetMapping("/register")
-    public String showRegisterForm(Model model) { // 회원 가입 페이지
-        model.addAttribute("user", new UserRegistrationDto());
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) { // 회원 가입 완료
+    @PostMapping("/api/register") // 회원 가입 API
+    public ResponseEntity<String> apiRegisterUserAccount(@RequestBody UserRegistrationDto registrationDto) {
         userService.registerUserAccount(registrationDto);
-        return "redirect:/register?success";
+        return new ResponseEntity<>("회원 가입이 완료되었습니다.", HttpStatus.CREATED);
     }
     
 }
--- src/main/java/project2/dto/UserRegistrationDto.java
+++ src/main/java/project2/dto/UserRegistrationDto.java
@@ -1,11 +1,15 @@
 package project2.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserRegistrationDto {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
+    @NotNull
     private String email;
     private String profileImageUrl;
     private String bio;
