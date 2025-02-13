91b2ce0f95c70f0016953996d566452beeb756e4
Bal1oon <ws32159@naver.com>
2025-02-13 14:29:58 +0900

    feat: Add posts and comments in user entity

--- src/main/java/project2/entity/Users.java
+++ src/main/java/project2/entity/Users.java
@@ -1,10 +1,15 @@
 package project2.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,6 +37,13 @@ public class Users extends BaseTimeEntity {
     private String profileImageUrl;
     
     private String bio;
+    
+    // 연관된 댓글들을 Cascade 및 orphanRemoval 옵션으로 관리
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Posts> posts = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comments> comments = new ArrayList<>();
     
     public void updateProfile(String username, String bio, String profileImageUrl) {
         this.username = username;
